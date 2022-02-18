46161ea3a8a8b16ec90c0bdb20661db93e1d61d7
Felipe Martins <felipe_mmmachado@hotmail.com>
2022-02-18 09:49:46 -0300

    First commit

---
+++ src/application/Program.java
@@ -0,0 +1,44 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exceptions.BusinessException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter account data");
+		System.out.print("Number: ");
+		int number = sc.nextInt();
+		sc.nextLine();
+		System.out.print("Holder: ");
+		String holder = sc.nextLine();
+		System.out.print("Initial balance: ");
+		double balance = sc.nextDouble();
+		System.out.print("Withdraw limit: ");
+		double limit = sc.nextDouble();
+		
+		Account acc = new Account(number, holder, balance, limit);
+		
+		System.out.println();
+		System.out.print("Enter amount for withdraw: ");
+		
+		try {
+			acc.withdraw(sc.nextDouble());
+			System.out.printf("New balance: %.2f",acc.getBalance());
+		}
+		catch (BusinessException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		sc.close();
+
+	}
+
+}
